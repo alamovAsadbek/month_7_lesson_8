f51ae72296a94591c65bb90f3e348a60087ffa1f
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-30 17:38:11 +0500

    add user phone number

--- app_users/models.py
+++ app_users/models.py
@@ -1,9 +1,6 @@
+from django.contrib.auth.base_user import AbstractBaseUser
 from django.db import models
 
 
-class UserModel(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    username = models.CharField(max_length=50)
-    phone_number = models.CharField(max_length=15)
-    password = models.CharField(max_length=100)
+class UserModel(AbstractBaseUser):
+    phone_number = models.CharField(max_length=15, unique=True)
