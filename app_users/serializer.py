b6c04dc01852c1c0ba21f730b64e3ec2b9b2a4bf
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-30 17:48:41 +0500

    change

---
+++ app_users/serializer.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from .models import *
+
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserModel
+        fields = '__all__'
