c8ed4ff31e1bf6894b49260f844e95823be4a4de
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-30 17:50:02 +0500

    change views.pyy

--- app_users/views.py
+++ app_users/views.py
@@ -1,11 +1,17 @@
+from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .serializer import *
 
 
 @api_view(['GET', 'POST'])
 def register_user(request):
     if request.method == 'POST':
-        # code to register user
-        pass
+        data = request.data
+        serializer = RegistrationSerializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
     else:
         # code to display registration form
         pass
