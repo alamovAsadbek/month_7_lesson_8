de2a250d0d80c7042db57bceaf23a9abd545fc55
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-30 17:51:11 +0500

    change

--- app_users/views.py
+++ app_users/views.py
@@ -5,12 +5,10 @@ from rest_framework.response import Response
 from .serializer import *
 
 
-@api_view(['GET', 'POST'])
+@api_view(['POST'])
 def register_user(request):
     if request.method == 'POST':
         data = request.data
         serializer = RegistrationSerializer(data=data)
         serializer.is_valid(raise_exception=True)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-    else:
-        return Response("Invalid request method", status=status.HTTP_400_BAD_REQUEST)
