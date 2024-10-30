eaba5babd18a9ebc27cf698d43c66768564f563f
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-30 17:34:42 +0500

    change

--- app_users/models.py
+++ app_users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class UserModel(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    username = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=15)
+    password = models.CharField(max_length=100)
