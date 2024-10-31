7b56c4657595601f87e9582497572afd47ae8253
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-31 21:40:57 +0500

    change

--- common/urls.py
+++ common/urls.py
@@ -5,4 +5,5 @@ from app_users.views import *
 urlpatterns = [
     path('login/', login_user_view, name='login_user_view'),
     path('register/', register_user_view, name='register_user_view'),
+    path('users/', get_user_view, name='get_user_view'),
 ]
