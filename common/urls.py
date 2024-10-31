c85e86ddde632efc8fc2fab9a082d9ee22580cc5
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-11-01 01:27:31 +0500

    change common/urls.py

--- common/urls.py
+++ common/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('login/', login_user_view, name='login_user_view'),
     path('register/', register_user_view, name='register_user_view'),
     path('users/', get_user_view, name='get_user_view'),
+    path('users/<int:user_id>/', get_user_by_id_view, name='get_user_by_id_view'),
 
 ]
