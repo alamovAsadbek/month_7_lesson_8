0fe60051248cbd8c6461059709c808e6c43aa2e8
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-31 22:07:14 +0500

    change

--- app_users/models.py
+++ app_users/models.py
@@ -22,6 +22,7 @@ class UserModel(AbstractBaseUser):
     phone_number = models.CharField(max_length=15, unique=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
 
     objects = UserManager()
 
