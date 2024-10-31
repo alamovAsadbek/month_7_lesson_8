d1a712cac7392d9d4332b2a2cb99920bb01f5ab9
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-31 23:40:15 +0500

    change

--- app_debts/views.py
+++ app_debts/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.decorators import permission_required
+from rest_framework.decorators import api_view
 from rest_framework.permissions import IsAuthenticated
 
 
+@api_view(['GET', 'POST'])
 @permission_required(IsAuthenticated)
-def create_debt_view(request):
+def debts_view(request):
     pass
