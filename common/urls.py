f7435ea73f74ec41ee487474911915f9ba560ef0
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-11-01 01:32:02 +0500

    change urls.py

--- common/urls.py
+++ common/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('users/', get_user_view, name='get_user_view'),
     path('users/<int:user_id>/', get_user_by_id_view, name='get_user_by_id_view'),
     path('debts/', debts_view, name='debts_view'),
-    path('my-borrowed-debts/', get_my_borrowed_view, name='get_my_borrowed_view')
+    path('my-borrowed-debts/', get_my_borrowed_view, name='get_my_borrowed_view'),
+    path('my-lent-debts/', get_my_lent_view, name='get_my_lent_view')
 ]
