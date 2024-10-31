576082f4264ac99dfa13830e82079d5ae5ba0c72
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-31 23:47:04 +0500

    change models.py

--- app_debts/models.py
+++ app_debts/models.py
@@ -1,5 +1,12 @@
+from django.db import models
+
+from app_users.models import UserModel
 from common.models import BaseModel
 
 
 class DebtView(BaseModel):
-    pass
+    DEBT_TYPE_CHOICES = [
+        ('borrowed', 'Borrowed'),
+        ('lent', 'Lent'),
+    ]
+    user = models.ForeignKey(UserModel, on_delete=models.SET_NULL, null=True)
