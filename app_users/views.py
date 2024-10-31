a22217cf6d6dc19f8e2fa1ef4f75513660788fc5
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-31 18:47:31 +0500

    change

--- app_users/views.py
+++ app_users/views.py
@@ -1,7 +1,7 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import *
+
 from .serializer import *
 
 
@@ -28,4 +28,8 @@ def login_user_view(request):
 @api_view(['GET'])
 def get_user_view(request):
     users = UserModel.objects.all()
+    search_query = request.query_params.get('search', None)
+    if search_query is not None:
+        users = users.filter(username__icontains=search_query)
     serializer = UserModelSerializer(users, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
