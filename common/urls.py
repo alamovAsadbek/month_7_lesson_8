26378cd29737f1291623b9b12811aeb8da3d33ba
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-31 21:39:59 +0500

    change

--- common/urls.py
+++ common/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from app_users.views import *
 
 urlpatterns = [
-    path('', login_user_view, name='login_user_view'),
+    path('login/', login_user_view, name='login_user_view'),
     path('register/', register_user_view, name='register_user_view'),
 ]
