da770866ee81e9eb4f3f8178e3938abd5a975c84
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-11-01 01:33:50 +0500

    add get-all-debts urls

--- common/urls.py
+++ common/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('debts/', debts_view, name='debts_view'),
     path('my-borrowed-debts/', get_my_borrowed_view, name='get_my_borrowed_view'),
     path('my-lent-debts/', get_my_lent_view, name='get_my_lent_view'),
-    path('inactive-debts/', inactivate_debt_view, name='inactivate_debt_view')
+    path('inactive-debts/', inactivate_debt_view, name='inactivate_debt_view'),
+    path('get-all-debts/', get_all_debts_view, name='get_all_debts_view')
 ]
