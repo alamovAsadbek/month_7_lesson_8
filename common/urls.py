fd362caad901b2d9356c160906e63ee569c1ee66
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-11-01 01:32:48 +0500

    add inactive-debts urls

--- common/urls.py
+++ common/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('users/<int:user_id>/', get_user_by_id_view, name='get_user_by_id_view'),
     path('debts/', debts_view, name='debts_view'),
     path('my-borrowed-debts/', get_my_borrowed_view, name='get_my_borrowed_view'),
-    path('my-lent-debts/', get_my_lent_view, name='get_my_lent_view')
+    path('my-lent-debts/', get_my_lent_view, name='get_my_lent_view'),
+    path('inactive-debts/', inactivate_debt_view, name='inactivate_debt_view')
 ]
