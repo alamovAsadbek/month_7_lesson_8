d316af9a1a3bad3ee7e4e0c2b3e4113b8d979ee7
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-11-01 01:15:41 +0500

    change debts_views.py

--- app_debts/views.py
+++ app_debts/views.py
@@ -11,6 +11,13 @@ from .serializer import *
 def debts_view(request):
     if request.method == 'GET':
         debts = DebtModel.objects.all(user=request.user)
+        page = request.query_params.get('page', 1)
+        paginator = request.query_params.get('paginator', 10)
+        search = request.query_params.get('search', None)
+        if page is not None:
+            debts = debts[(int(page) - 1) * int(paginator):int(page) * int(paginator)]
+        if search is not None:
+            debts = debts.filter(description__icontains=search)
         serializer = DebtSerializer(debts, many=True)
         return Response(serializer.data)
     elif request.method == 'POST':
