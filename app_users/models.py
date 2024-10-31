d0495b3e11f02e44ccda0f5ee5a4bb3d5e8f0ea7
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-31 21:55:15 +0500

    change

--- app_users/models.py
+++ app_users/models.py
@@ -4,3 +4,7 @@ from django.db import models
 
 class UserModel(AbstractBaseUser):
     phone_number = models.CharField(max_length=15, unique=True)
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
