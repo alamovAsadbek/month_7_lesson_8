b7afaef1d2c4a29a0029daa03ef342a4051d6be0
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-31 23:55:51 +0500

    change

--- app_debts/views.py
+++ app_debts/views.py
@@ -1,12 +1,18 @@
 from django.contrib.auth.decorators import permission_required
 from rest_framework.decorators import api_view
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+from .serializer import *
 
 
 @api_view(['GET', 'POST'])
 @permission_required(IsAuthenticated)
 def debts_view(request):
     if request.method == 'GET':
-        pass
+        debts = DebtModel.objects.all(user=request.user)
+        serializer = DebtSerializer(debts, many=True)
+        return Response(serializer.data)
+
     elif request.method == 'POST':
         pass
